42b89b6abfb8180f45559b8ff81b63223b839574
joriskalz <joris@kalz.de>
2020-05-22 14:48:36 +0200

    Changed Header

--- react-app/src/components/HeaderBarBrand.js
+++ react-app/src/components/HeaderBarBrand.js
@@ -12,9 +12,9 @@ const HeaderBarBrand = () => (
       <i className="fab js-logo fa-react fa-2x" aria-hidden="true" />
     </a>
     <NavLink to="/" className="navbar-item nav-home">
-      <span className="brand-first">MY</span>
-      <span className="brand-second">SHOPPING</span>
-      <span className="brand-third">LIST</span>
+      <span className="brand-first">THIS IS MY SHOPPING LIST</span>
+      {/* <span className="brand-second">SHOPPING</span>
+      <span className="brand-third">LIST</span> */}
     </NavLink>
   </div>
 );
